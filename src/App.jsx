600f635b619bd693f93659715cf53a16ddfad8a8
Marco Ye <marco.ye99@gmail.com>
2022-07-18 17:31:57 -0400

    set up router paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-   
-    </div>
+    <Router>
+      <Switch>
+        <Redirect from='/' to= '/warehouses'/>
+        <Route path= '/warehouses' component= {placeHolder}/>
+        <Route path= '/warehouses/:warehouseId' component= {placeHolder}/>
+        <Route path='/inventory' component= {placeHolder}/>
+        <Route path='/inventory/:inventoryId' component= {placeHolder}/>
+      </Switch>
+    </Router>
   );
 }
 
