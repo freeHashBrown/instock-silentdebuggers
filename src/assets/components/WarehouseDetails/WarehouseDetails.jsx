a71b983c5d87f420f9ed03302158070e25fe5612
Jigar Patel <jigar@JP-machine.local>
2022-07-19 19:06:45 -0400

    Add warehouse component files

---
+++ src/assets/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function WarehouseDetails() {
+    return (
+        <div className='warehousedetails'>
+            <div className='warehousedetails__container'>
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default WarehouseDetails;
