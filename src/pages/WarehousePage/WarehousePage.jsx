a701ce85bb1929409d1a60a9d7c8e076bcc2653a
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-19 17:35:46 -0400

    adjust warehouse page title with rems, import and use class from typography partial

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -8,14 +8,14 @@ class WarehousePage extends Component {
     render() {
         return (
             <main className='warehouse-page'>
-                <header className='warehouse-page__header'>
-                    <h1 className='warehouse-page__title'>
+                <header className='warehouse-page__header page-header'>
+                    <h1 className='warehouse-page__title page-header'>
                         Warehouses
                     </h1>
                 </header>
                 <article className='warehouse-page__top'>
                     <section className='warehouse-page__search-bar'>
-                        <input type="text" placeholder='Search...' className='warehouse-page__input'/>
+                        <input type="text" placeholder='Search...' className='warehouse-page__input label-text'/>
                         <img src={searchIcon} alt="search icon" className='warehouse-page__search-icon'/>
                     </section>
                     <section>
