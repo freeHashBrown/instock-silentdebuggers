5b3a30371a5466529da1fc0982d1dbba8d70c7cb
Marco Ye <marco.ye99@gmail.com>
2022-07-21 04:10:09 -0400

    completed visual layouts; functionality still missing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom
 import Header from '../src/components/Header/Header'
 import AddNewWarehouse from '../src/components/AddNewWarehouse/AddNewWarehouse';
 import WarehouseDetails from './components/WarehouseDetails/WarehouseDetails';
-import DeleteInventory from './components/DeleteInventory/DeleteInventory';
+import AddNewInventory from './components/AddNewInventory/AddNewInventory';
+
 
 
 function App() {
   return (
     <Router>
       <Header />
+      <AddNewInventory/>
       <Switch>
         <Redirect from='/' to= '/warehouses'/>
         <Route path= '/warehouses'/>
