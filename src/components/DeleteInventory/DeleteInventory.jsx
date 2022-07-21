a94f0446c627d546387960f7a162e000f60c95ac
Marco Ye <marco.ye99@gmail.com>
2022-07-21 11:06:57 -0400

    fixed heading; still missing functionality

--- src/components/DeleteInventory/DeleteInventory.jsx
+++ src/components/DeleteInventory/DeleteInventory.jsx
@@ -9,7 +9,7 @@ function DeleteInventory() {
             </div>
             <div className='deleteInventory__text-container'>
                 <h1 className='deleteInventory__heading page-header'>
-                    Delete Television warehouse?
+                    Delete Television inventory item?
                 </h1>
                 <p className='deleteInventory__text body-large'>
                     Please confirm that you'd like to delete Television from the inventory list. You won't be able to undo this action.
