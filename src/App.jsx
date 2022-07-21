81b8423c1b517aba18144a4b9a7bb5e5fa5625c9
Marco Ye <marco.ye99@gmail.com>
2022-07-21 01:34:20 -0400

    removed delete inventory for now

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <Router>
       <Header />
-      <DeleteInventory />
       <Switch>
         <Redirect from='/' to= '/warehouses'/>
         <Route path= '/warehouses'/>
