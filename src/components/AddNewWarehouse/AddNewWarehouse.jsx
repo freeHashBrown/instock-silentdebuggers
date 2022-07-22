76cc9861883e52d3e4130fd937eb1ccef32c6f7f
Marco Ye <marco.ye99@gmail.com>
2022-07-22 01:15:52 -0400

    working form validation and post

--- src/components/AddNewWarehouse/AddNewWarehouse.jsx
+++ src/components/AddNewWarehouse/AddNewWarehouse.jsx
@@ -1,5 +1,6 @@
 import './AddNewWarehouse.scss';
 import backIcon from '../../assets/icons/arrow_back-24px.svg'
+import axios from 'axios';
 
 function AddNewWarehouse () {
 
@@ -11,8 +12,44 @@ function AddNewWarehouse () {
             if (!formInputs) {
                 e.target[i].nextSibling.classList.remove('addNewWarehouse__input-error--hidden')
                 e.target[i].nextSibling.classList.add('addNewWarehouse__input-error')
+            } else {
+                e.target[i].nextSibling.classList.add('addNewWarehouse__input-error--hidden');
+                // axios.post(`http://localhost:8080/warehouses`, {
+                //     name: e.target[0].value,
+                //     address: e.target[1].value,
+                //     city: e.target[2].value,
+                //     country: e.target[3].value,
+                //     contactName: e.target[4].value,
+                //     position: e.target[5].value,
+                //     phone: e.target[6].value,
+                //     email: e.target[7].value
+                    
+                // })
+                //     .catch((error) => {'Error uploading'})
             }
         }
+        if (!e.target[0].value || !e.target[1].value || !e.target[2].value || !e.target[3].value || !e.target[4].value || !e.target[5].value || !e.target[6].value || !e.target[7].value) {
+            return
+        } else {
+            axios.post(`http://localhost:8080/warehouses`, {
+                name: e.target[0].value,
+                address: e.target[1].value,
+                city: e.target[2].value,
+                country: e.target[3].value,
+                contactName: e.target[4].value,
+                position: e.target[5].value,
+                phone: e.target[6].value,
+                email: e.target[7].value
+            })
+            e.target.reset();
+            successMessage();
+        }
+    }
+
+    const successMessage = () => {
+        const message = document.getElementById('success');
+        message.classList.remove('addNewWarehouse__success--hidden');
+        message.classList.add('addNewWarehouse__success');
     }
 
     const handleClick = () => {
@@ -87,6 +124,7 @@ function AddNewWarehouse () {
                     </form>
                 </main>
                 <div className='addNewWarehouse__button-container'>
+                    <label className='addNewWarehouse__success--hidden' id='success'>Warehouse added!</label>
                     <button className='addNewWarehouse__button-cancel button-text' type='submit' onClick={handleClick}>
                         Cancel
                     </button>
