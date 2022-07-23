5f21b91bfcea000f4ede3c35cae41fadd2d17aea
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-23 15:31:30 -0400

    Create basic structure of header

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./InventoryItemDetails.scss";
+import arrowBack from "../../assets/icons/arrow_back-24px.svg";
+import editIcon from "../../assets/icons/edit_white_24dp.svg";
 
 const InventoryItemDetails = () => {
     return (
         <article>
-            
+            <header>
+                <img src={arrowBack} alt="Arrow Back" />
+                <h1>Television</h1>
+                <img src={editIcon} alt="Edit" />
+            </header>
         </article>
     );
 };
