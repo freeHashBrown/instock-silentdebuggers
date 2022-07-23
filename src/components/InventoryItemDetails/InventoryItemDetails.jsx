06c82c89e85065b70e4b4bb16f15240b1573772b
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-23 16:00:02 -0400

    Complete header for the mobile version of website

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -5,13 +5,22 @@ import editIcon from "../../assets/icons/edit_white_24dp.svg";
 
 const InventoryItemDetails = () => {
     return (
-        <article>
-            <header>
-                <img src={arrowBack} alt="Arrow Back" />
-                <h1>Television</h1>
-                <img src={editIcon} alt="Edit" />
-            </header>
-        </article>
+        <main className='inventory-details'>
+            <article className='inventory-details__article'>
+                <header className='inventory-details__header'>
+                    <div className='inventory-details__left'>
+                        <img src={arrowBack} alt="Arrow Back" className='inventory-detials__back'/>
+                        <h1 className='inventory-details__title page-header'>
+                        Television
+                        </h1>
+                    </div>
+                    <section className='inventory-details__container'>
+                        <img src={editIcon} alt="Edit" className='inventory-details__edit'/>
+                    </section>
+                </header>
+            </article>
+        </main>
+        
     );
 };
 
