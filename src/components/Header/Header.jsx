9600efa69ef350c5c1956d4e046b311ba1468e31
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-23 18:53:15 -0400

    Add functions to header buttons to link to differenct pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,32 @@
 import './Header.scss';
 import logo from '../../assets/logo/InStock-Logo.svg'
+import { Link, useHistory } from "react-router-dom";
 
 function Header () {
-    // const handleClick = (event) => {
-    //     event.target.classList.add('header__nav-item--selected');
-    //     event.target.nextSibling.classList.remove('header__nav-item--selected');
-    // }
-    // const handleClickTwo = (event) => {
-    //     event.target.classList.add('header__nav-item--selected');
-    //     event.target.previousSibling.classList.remove('header__nav-item--selected');
-    // }
+
+    //Create history object to go back to home page
+    const history = useHistory();
+
+    const handleClick = (event) => {
+        event.target.classList.add('header__nav-item--selected');
+        event.target.nextSibling.classList.remove('header__nav-item--selected');
+        history.push("/warehouses")
+    }
+
+    const handleClickTwo = (event) => {
+        event.target.classList.add('header__nav-item--selected');
+        event.target.previousSibling.classList.remove('header__nav-item--selected');
+        history.push("/inventory");
+    }
 
     return ( 
         <div className='header'>
             <img className='header__logo' src={logo} alt='logo'/>
             <ul className='header__nav'>
-                <li className='header__nav-item button-text'>
+                <li className='header__nav-item button-text' onClick={handleClick}>
                     Warehouses
                 </li>
-                <li className='header__nav-item button-text'>
+                <li className='header__nav-item button-text' onClick={handleClickTwo}>
                     Inventory
                 </li>
             </ul>
